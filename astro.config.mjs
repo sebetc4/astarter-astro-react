a46ea6c563ed065fe0f4fc90b2bc631fb37de203
sebetc4 <sebastienetcheto@gmail.com>
2024-10-17 23:42:28 +0200

    core: update tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,25 @@
 import react from '@astrojs/react'
 import { defineConfig } from 'astro/config'
 import cssnano from 'cssnano'
+import variableCompress from 'postcss-variable-compress'
+import sitemap from '@astrojs/sitemap';
+
+const isProd = import.meta.env.PROD;
+
+console.log('isProd', isProd);
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
+    integrations: [react(), sitemap()],
     vite: {
         resolve: {
             alias: {
                 '@': '/src',
-                '@react-comp': '/src/components/react',
-                '@astro-comp': '/src/components/astro',
             },
         },
         css: {
             postcss: {
-                plugins: [cssnano()],
+                plugins: isProd ? [cssnano(), variableCompress()] : [],
             },
             preprocessorOptions: {
                 scss: {
@@ -25,4 +29,5 @@ export default defineConfig({
             },
         },
     },
+    site: 'https://my-site.com',
 })
