72d5c7d5810dfa199483b359fe6951f91acded29
sebetc4 <sebastienetcheto@gmail.com>
2024-10-18 22:50:40 +0200

    core: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,95 +1,35 @@
-import eslint from '@eslint/js'
-import astroParser from 'astro-eslint-parser'
+import js from '@eslint/js'
 import prettier from 'eslint-config-prettier'
-import eslintPluginAstro from 'eslint-plugin-astro'
-import eslintPluginPrettier from 'eslint-plugin-prettier'
-import eslintPluginReact from 'eslint-plugin-react'
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
-import eslintPluginReactRefresh from 'eslint-plugin-react-refresh'
+import pluginAstro from 'eslint-plugin-astro'
+import pluginPrettier from 'eslint-plugin-prettier'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 
-const commonRules = {
-    'prettier/prettier': 'warn',
-    '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-            vars: 'all',
-            args: 'all',
-            ignoreRestSiblings: false,
-            argsIgnorePattern: '^_',
-        },
-    ],
-}
 
-export default [
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
+export default tseslint.config(
     {
+        files: ['**/*.{ts,tsx}'],
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
         languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.browser,
-            },
-            parserOptions: {
-                ecmaVersion: 'latest',
-                sourceType: 'module',
-            },
-        },
-    },
-    {
-        files: ['**/*.{js,jsx,ts,tsx}'],
-        languageOptions: {
-            parser: tseslint.parser,
+            ecmaVersion: 2020,
+            globals: globals.browser,
         },
         plugins: {
-            '@typescript-eslint': tseslint.plugin,
-            react: eslintPluginReact,
-            'react-hooks': eslintPluginReactHooks,
-            'react-refresh': eslintPluginReactRefresh,
-            prettier: eslintPluginPrettier,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            prettier: pluginPrettier,
         },
         rules: {
-            ...eslintPluginReact.configs.recommended.rules,
-            ...eslintPluginReactHooks.configs.recommended.rules,
-            ...eslintPluginPrettier.configs.recommended.rules,
-            ...commonRules,
+            ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-            'react/prop-types': 'off',
-            'react/react-in-jsx-scope': 'off',
-            'react-hooks/exhaustive-deps': 'warn',
-        },
-        settings: {
-            react: {
-                version: 'detect',
-            },
-            'import/resolver': {
-                typescript: {},
-            },
+            'prettier/prettier': 'warn',
         },
     },
-    {
-        files: ['**/*.astro'],
-        languageOptions: {
-            parser: astroParser,
-            parserOptions: {
-                parser: '@typescript-eslint/parser',
-                extraFileExtensions: ['.astro'],
-            },
-        },
-        plugins: {
-            astro: eslintPluginAstro,
-            prettier: eslintPluginPrettier,
-        },
-        rules: {
-            ...eslintPluginAstro.configs.recommended.rules,
-            ...eslintPluginPrettier.configs.recommended.rules,
-            ...commonRules,
-            'astro/no-set-html-directive': 'error',
-        },
-    },
-    {
-        ignores: ['dist/**', '.astro/**', 'node_modules/**'],
-    },
+    jsxA11y.flatConfigs.recommended,
+    ...pluginAstro.configs.recommended,
     prettier,
-]
+    { ignores: ['dist', 'node_modules'] },
+)
